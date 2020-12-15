9576977c413229912cc626cda2edfa6841b391bc
kunle niyi <58327707+adekniyi@users.noreply.github.com>
2020-12-15 17:48:01 +0100

    working with database

--- Vidly App/Controllers/CustomerController.cs
+++ Vidly App/Controllers/CustomerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly_App.Data;
 using Vidly_App.Models;
 using Vidly_App.ViewModel;
 
@@ -10,47 +12,64 @@ namespace Vidly_App.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: Customers
         [Route("Customers")]
         public ActionResult Index()
         {
-            var customer = new List<Customer>
-            {
-                new Customer {Name = "John Smith",  Id = 1 },
-                new Customer {Name = "Mary William",  Id = 2}
-            };
+            //var customer = new List<Customer>
+            //{
+            //    new Customer {Name = "John Smith",  Id = 1 },
+            //    new Customer {Name = "Mary William",  Id = 2}
+            //};
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Customer = customer
-            };
-            return View(viewModel);
+            //var viewModel = new RandomMovieViewModel
+            //{
+            //    Customer = customer
+            //};
+
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customer);
         }
 
         [Route("Customers/Details/{id}")]
         public ActionResult Details(int id)
         {
 
-            var customer = new List<Customer>
-            {
-                new Customer {Name = "John Smith" ,  Id = 1 },
-                new Customer {Name = "Mary William",  Id = 2}
-            };
+            //var customer = new List<Customer>
+            //{
+            //    new Customer {Name = "John Smith" ,  Id = 1 },
+            //    new Customer {Name = "Mary William",  Id = 2}
+            //};
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Customer = customer
-            };
+            //var viewModel = new RandomMovieViewModel
+            //{
+            //    Customer = customer
+            //};
 
 
-            foreach (var custom in viewModel.Customer)
+            //foreach (var custom in viewModel.Customer)
+            //{
+            //    if (custom.Id == id)
+            //    {
+            //        return View(custom);
+            //    }
+            //}
+            //return NotFound();
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
             {
-                if (custom.Id == id)
-                {
-                    return View(custom);
-                }
+                return NotFound();
             }
-            return NotFound();
+
+            return View(customer);
         }
 
     }
--- Vidly App/Data/ApplicationDbContext.cs
+++ Vidly App/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Vidly_App.Models;
 
 namespace Vidly_App.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
