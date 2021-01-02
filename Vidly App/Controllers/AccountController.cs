26b22e036a560ca2738b8455909f8650d310c1de
kunle niyi <58327707+adekniyi@users.noreply.github.com>
2021-01-02 20:56:05 +0100

    Setting up account controller

---
+++ Vidly App/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Vidly_App.Data;
+using Vidly_App.Models;
+using System.Web;
+using Microsoft.Owin.Host.SystemWeb;
+//using System.Web.Mvc;
+using System.Net.Http;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+//using System.Web.Mvc;
+
+namespace Vidly_App.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private SignInManager<IdentityUser> _signInManager;
+        private Microsoft.AspNetCore.Identity.UserManager<IdentityUser> _userManager;
+
+        public AccountController(Microsoft.AspNetCore.Identity.UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+
+
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [Route("Account/Register")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                //AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Vidly App/Data/ApplicationDbContext.cs
+++ Vidly App/Data/ApplicationDbContext.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Vidly_App.Models;
 
 namespace Vidly_App.Data
 {
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        //{
+        //    // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        //    var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        //    // Add custom user claims here
+        //    return userIdentity;
+        //}
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> manager)
+        {
+            var authenticationType = "Put authentication type Here";
+            var userIdentity = new ClaimsIdentity(await manager.GetClaimsAsync(this), authenticationType);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Customer> Customers { get; set; }
