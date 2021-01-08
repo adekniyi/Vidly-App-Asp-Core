5ebfddf506d663504e650e4a1aea30d3d0995232
kunle niyi <58327707+adekniyi@users.noreply.github.com>
2021-01-08 09:29:12 +0100

    fixing the customer autocomplete code

---
+++ Vidly App/Controllers/CustomerRestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly_App.Data;
+
+namespace Vidly_App.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerRestController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerRestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("search")]
+        public async Task<IActionResult> Search()
+        {
+            try
+            {
+                string term = HttpContext.Request.Query["term"].ToString();
+                var name = _context.Customers.Where(p => p.Name.Contains(term))
+                    .Select(p => p.Name).ToList();
+                return Ok(name);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
